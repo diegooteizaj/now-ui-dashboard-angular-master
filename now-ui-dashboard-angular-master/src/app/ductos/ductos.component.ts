2be94b1fc184e4528b3d323c5e50ad9ee7a54ae1
diegooteizaj <diego.oteiza@gmail.com>
2024-01-16 19:06:26 -0300

    qrPdf

--- now-ui-dashboard-angular-master/src/app/ductos/ductos.component.ts
+++ now-ui-dashboard-angular-master/src/app/ductos/ductos.component.ts
@@ -4,6 +4,7 @@ import { Utils } from '../utils';
 import { Observable } from 'rxjs';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-ductos',
@@ -62,11 +63,70 @@ export class DuctosComponent implements OnInit {
     this.modalQr = false;
   }
 
-  public downloadPDF(id:any): void {
+  // public downloadPDF(id:any): void {
+  //   const doc = new jsPDF();
+
+  //   doc.text('Hello world!', 10, 10);
+  //   doc.save(`Qr_Ducto_${id}.pdf`);
+  // }
+
+  // downloadPDF(id: any): void {
+  //   const doc = new jsPDF();
+
+  //   doc.text(`Ducto ${id}`, 70, 10, { align: 'center' });
+  //   // Generar el código QR
+  //   const qrData =  this.ductoUrl; // Reemplaza esto con la URL real de tu aplicación
+  //   QRCode.toDataURL(qrData, { errorCorrectionLevel: 'H' }, function (err, url) {
+  //     if (err) {
+  //       console.error('Error al generar el código QR:', err);
+  //       return;
+  //     }
+
+  //      // Obtener dimensiones de la imagen
+  //      const imgWidth = 100; // ajusta según tus necesidades
+  //      const imgHeight = 100; // ajusta según tus necesidades
+ 
+  //      // Calcular coordenadas para centrar la imagen con un margen superior de 30px
+  //      const x = (doc.internal.pageSize.getWidth() - imgWidth) / 2;
+  //      const y = 40; // Margen superior de 30px
+ 
+  //      // Insertar el código QR en el documento PDF
+  //      doc.addImage(url, 'JPEG', x, y, imgWidth, imgHeight);
+ 
+  //      // Guardar el documento PDF
+  //      doc.save(`Qr_Ducto_${id}.pdf`);
+  //   });
+  // }
+
+
+  downloadPDF(id: any): void {
     const doc = new jsPDF();
 
-    doc.text('Hello world!', 10, 10);
-    doc.save(`Qr_Ducto_${id}.pdf`);
+    // Agregar título
+    const title = `Ducto ${id}`;
+    doc.text(title, doc.internal.pageSize.getWidth() / 2, 10, { align: 'center' });
+
+    // Generar el código QR
+    QRCode.toDataURL(this.ductoUrl + `/${id}`, { errorCorrectionLevel: 'H' }, function (err, url) {
+      if (err) {
+        console.error('Error al generar el código QR:', err);
+        return;
+      }
+
+      // Obtener dimensiones de la imagen
+      const imgWidth = 100; // ajusta según tus necesidades
+      const imgHeight = 100; // ajusta según tus necesidades
+
+      // Calcular coordenadas para centrar la imagen debajo del título
+      const x = (doc.internal.pageSize.getWidth() - imgWidth) / 2;
+      const y = 30; // Margen superior de 30px desde la posición del título
+
+      // Insertar el código QR en el documento PDF
+      doc.addImage(url, 'JPEG', x, y, imgWidth, imgHeight);
+
+      // Guardar el documento PDF
+      doc.save(`Qr_Ducto_${id}.pdf`);
+    });
   }
 
 
